0199e0a16ec1c724acb31a9fcdf06db98c995a0a
Fnu Sameer <sameersah7365@gmail.com>
2024-10-01 18:29:21 -0700

    added helloworld Main.java

---
+++ src/main/java/helloworld/Main.java
@@ -0,0 +1,8 @@
+package main.java.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a standalone java project and yes, Hello world!");
+    }
+}
